91b6a3c22d9ec11f2df55a5181026f48b4832eee
AtriAhmed <atriahmed.1999@gmail.com>
2025-09-27 11:25:54 +0300

    next config image url

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,20 @@ import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.molinel.com",
+        pathname: "/**",
+      },
+    ],
+    // If you prefer the simpler form, you could use:
+    // domains: ["www.molinel.com"],
+  },
+  // other config options...
 };
 
 const withNextIntl = createNextIntlPlugin();
+
 export default withNextIntl(nextConfig);
