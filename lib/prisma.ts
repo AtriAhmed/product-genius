2170bcfef45497137ca102bdbf9abe3afa20e036
Omar Atri <atriomar00@gmail.com>
2025-11-22 09:47:40 +0100

    Update prisma.ts file

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,10 @@
 import { PrismaClient } from "@/app/generated/prisma/client";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
+};
+
 const adapter = new PrismaMariaDb({
   host: process.env.DB_HOST || "localhost",
   port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
@@ -10,4 +14,12 @@ const adapter = new PrismaMariaDb({
   connectionLimit: 5,
 });
 
-export const prisma = new PrismaClient({ adapter });
+const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter,
+  });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export { prisma };
